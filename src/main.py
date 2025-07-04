ebe32bf7b68df9b00a1d3a5460551254b096f658
Cameron Robinson <cameron@ckrobinson.net>
2025-07-04 13:27:54 -0700

    Update to direct to docs for ghpages.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import os
 from os.path import isdir
 import shutil
@@ -7,11 +8,11 @@ from markdown_handler import markdown_to_html
 from page_hander import extract_title
 
 source="static"
-target="public"
+target="docs"
 
-def deletePublic():
+def deletePublic(target: str):
     if os.path.exists(f"./{target}"):
-        print("Cleaning existing public directory.")
+        print(f"Cleaning existing {target} directory.")
         shutil.rmtree(f"./{target}")
 
 def copyDirectory(source, target, relativePath: str = ""):
@@ -28,7 +29,7 @@ def copyDirectory(source, target, relativePath: str = ""):
         if os.path.isfile(source + itemPath):
             shutil.copy(source + itemPath, target + itemPath)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, rootWebPath=""):
     print(f"Generating page from {from_path} to {dest_path}, using {template_path}")
 
     if os.path.exists(from_path) == False:
@@ -55,11 +56,17 @@ def generate_page(from_path, template_path, dest_path):
     final_content = re.sub(r"{{ Title }}", title, template_content)
     final_content = re.sub(r"{{ Content }}", htmlString, final_content)
 
+    if rootWebPath != "":
+        rootWebPath = f"{rootWebPath}/"
+
+    final_content = re.sub(r"href=\"/", f"href=\"/{rootWebPath}", final_content)
+    final_content = re.sub(r"src=\"/", f"src=\"/{rootWebPath}", final_content)
+
     file = open(dest_path, 'w')
     file.write(final_content)
     file.close()
 
-def generate_pages_recursive(source, template_path, target):
+def generate_pages_recursive(source, template_path, target, rootWebPath=""):
     def recurse(relativePath: str = ""):
         if os.path.exists(target + relativePath) == False:
             os.mkdir(target + relativePath)
@@ -75,12 +82,16 @@ def generate_pages_recursive(source, template_path, target):
             result = re.match(r"(.+)\.md", item)
             if os.path.isfile(source + itemPath) and result:
                 targetItem = f"{result.groups()[0]}.html"
-                generate_page(source+itemPath, template_path, target+relativePath+"/"+targetItem)
+                generate_page(source+itemPath, template_path, target+relativePath+"/"+targetItem, rootWebPath)
 
     recurse()
 
 if __name__ == '__main__':
-    deletePublic()
+    rootWebPath = ""
+    if len(sys.argv) > 1:
+        rootWebPath = sys.argv[1]
+
+    deletePublic(target)
     os.mkdir(f"{target}")
     copyDirectory(f"./{source}", f"./{target}")
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", target, rootWebPath)
